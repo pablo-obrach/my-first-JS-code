d0e8d9f2420698e31465e012369bb005e8aae432
Pablo Obrach <hernanpabloobrach88@gmail.com>
2023-05-31 20:12:03 -0300

    uploading the main files of the challenge

---
+++ main.js
@@ -0,0 +1,22 @@
+const hero = document.querySelector("#hero");
+let contadorClicks = 0;
+let estadoOriginal = null;
+
+hero.addEventListener("click", () => {
+  contadorClicks++;
+  if (contadorClicks === 1) {
+    hero.classList.add("face-right");
+  } else if (contadorClicks === 2) {
+    hero.classList.remove("face-right");
+    hero.classList.add("face-left");
+  } else if (contadorClicks === 3) {
+    hero.classList.remove("face-left");
+    hero.classList.add("face-up");
+    estadoOriginal = {
+      contador: contadorClicks - 3,
+    };
+  } else {
+    hero.classList.remove("face-up");
+    contadorClicks = estadoOriginal.contador + 1;
+  }
+});
