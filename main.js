2e6d1543e00d328a8c8e862e63c75522835d77a0
Pablo Obrach <hernanpabloobrach88@gmail.com>
2023-05-31 20:46:33 -0300

    improving the direction changes on the JS code

--- main.js
+++ main.js
@@ -8,15 +8,15 @@ hero.addEventListener("click", () => {
     hero.classList.add("face-right");
   } else if (contadorClicks === 2) {
     hero.classList.remove("face-right");
-    hero.classList.add("face-left");
-  } else if (contadorClicks === 3) {
-    hero.classList.remove("face-left");
     hero.classList.add("face-up");
+  } else if (contadorClicks === 3) {
+    hero.classList.remove("face-up");
+    hero.classList.add("face-left");
     estadoOriginal = {
       contador: contadorClicks - 3,
     };
   } else {
-    hero.classList.remove("face-up");
+    hero.classList.remove("face-left");
     contadorClicks = estadoOriginal.contador;
   }
 });
