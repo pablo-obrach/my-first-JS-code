1319494fc6159485a050c340b9df38f2d448e020
Pablo Obrach <hernanpabloobrach88@gmail.com>
2023-06-01 18:00:34 -0300

    optimization of the code for the animation rotation

--- main.js
+++ main.js
@@ -5,18 +5,36 @@ let estadoOriginal = null;
 hero.addEventListener("click", () => {
   contadorClicks++;
   if (contadorClicks === 1) {
-    hero.classList.add("face-right");
+    hero.classList.toggle("face-right");
   } else if (contadorClicks === 2) {
-    hero.classList.remove("face-right");
-    hero.classList.add("face-up");
+    hero.classList.toggle("face-up");
   } else if (contadorClicks === 3) {
-    hero.classList.remove("face-up");
-    hero.classList.add("face-left");
+    hero.classList.toggle("face-left");
     estadoOriginal = {
       contador: contadorClicks - 3,
     };
   } else {
-    hero.classList.remove("face-left");
+    hero.classList.remove("face-left", "face-up", "face-right");
     contadorClicks = estadoOriginal.contador;
   }
 });
+
+//ESTE FUE EL PRIMER METODO QUE SE ME OCURRIO LUEGO LO OPTIMICÉ
+// hero.addEventListener("click", () => {
+//   contadorClicks++;
+//   if (contadorClicks === 1) {
+//     hero.classList.add("face-right");
+//   } else if (contadorClicks === 2) {
+//     hero.classList.remove("face-right");
+//     hero.classList.add("face-up");
+//   } else if (contadorClicks === 3) {
+//     hero.classList.remove("face-up");
+//     hero.classList.add("face-left");
+//     estadoOriginal = {
+//       contador: contadorClicks - 3,
+//     };
+//   } else {
+//     hero.classList.remove("face-left");
+//     contadorClicks = estadoOriginal.contador;
+//   }
+// });
